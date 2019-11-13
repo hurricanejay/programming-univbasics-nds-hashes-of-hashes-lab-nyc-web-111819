8bb57578f52de1e0b0abfb984358ad9fb890e417
Jay Wen <jaywen89@gmail.com>
2019-11-13 18:44:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,7 +48,8 @@ def naming_system
       label: "Phylum",
       sub_category: {
         label: "Class",
-        sub_category: "Order"
+        sub_category: "Order",
+        
   },
     :Family=> {}
       :Genus=> {}
